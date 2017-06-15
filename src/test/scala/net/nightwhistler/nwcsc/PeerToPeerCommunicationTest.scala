9773df08bfe1310fea8fdd37fbd649966509a9b2
Alex Kuiper <alex@nightwhistler.net>
2017-06-15 13:48:39 +0200

    Another testcase

--- src/test/scala/net/nightwhistler/nwcsc/PeerToPeerCommunicationTest.scala
+++ src/test/scala/net/nightwhistler/nwcsc/PeerToPeerCommunicationTest.scala
@@ -8,24 +8,40 @@ import org.scalatest.{FunSuite, GivenWhenThen}
   */
 class PeerToPeerCommunicationTest extends FunSuite with GivenWhenThen with MockFactory {
 
-  trait MockedBroadcast {
-    val broadcastStub = mockFunction[PeerMessage, Unit]
+  trait StubbedBroadcast {
+    val broadcastStub = stubFunction[PeerMessage, Unit]
     def broadcast(peerMessage: PeerMessage): Unit = broadcastStub(peerMessage)
   }
 
+  abstract class SimplePeerToPeerCommunication(val blockChain: BlockChain) extends PeerToPeerCommunication
+
   test("A query all should lead to a reply of the full blockchain") {
     Given("a basic blockchain with 1 block")
-    val peerToPeerCommunication = new PeerToPeerCommunication with MockedBroadcast {
-      override def blockChain: BlockChain = BlockChain()
-    }
+    val blockChain = BlockChain()
+    val peerToPeerCommunication = new SimplePeerToPeerCommunication(blockChain) with StubbedBroadcast
+
     val reply = stubFunction[PeerMessage, Unit]
-    peerToPeerCommunication.blockChain.addBlock("My test data")
+    blockChain.addBlock("My test data")
 
     When("we query for the full chain")
     peerToPeerCommunication.handleMessage(PeerMessage(MessageType.QueryAll), reply)
 
     Then("We expect the full blockchain back")
-    reply.verify(PeerMessage(MessageType.ResponseBlockChain, peerToPeerCommunication.blockChain.blocks))
+    reply.verify(PeerMessage(MessageType.ResponseBlockChain, blockChain.blocks))
+  }
 
+  test("Querying for the latest block should yield the latest block, and nothing more.") {
+    Given("a basic blockchain with 1 block")
+    val blockChain = BlockChain()
+    val peerToPeerCommunication = new SimplePeerToPeerCommunication(blockChain) with StubbedBroadcast
+
+    val reply = stubFunction[PeerMessage, Unit]
+    blockChain.addBlock("My test data")
+
+    When("we query for a single block")
+    peerToPeerCommunication.handleMessage(PeerMessage(MessageType.QueryLatest), reply)
+
+    Then("We only expect the latest block")
+    reply.verify( PeerMessage(MessageType.ResponseBlockChain, Seq(blockChain.getLatestBlock)))
   }
 }
